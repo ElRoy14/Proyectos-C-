b0e4e607fbd0ebc43dc18d5960657a7b78bf4ab6
Roylin <roylinrodriguez5@gmail.com>
2023-06-07 18:14:42 -0400

    Proceso CRUD

---
+++ CRUD_Almacen/CRUD_Almacen.Presentacion/DataSet_Datos.cs
@@ -0,0 +1,16 @@
+﻿namespace CRUD_Almacen.Presentacion
+{
+}
+
+namespace CRUD_Almacen.Presentacion {
+    
+    
+    public partial class DataSet_Datos {
+    }
+}
+namespace CRUD_Almacen.Presentacion {
+    
+    
+    public partial class DataSet_Datos {
+    }
+}
